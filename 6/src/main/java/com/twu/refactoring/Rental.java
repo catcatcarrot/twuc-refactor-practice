72b19a00f02ab8a34ee1312999ab269606883d3a
yan.zhao <catcatcarrot1207@163.com>
2021-08-15 11:10:51 +0800

    feature envy: extact methods to rental

--- 6/src/main/java/com/twu/refactoring/Rental.java
+++ 6/src/main/java/com/twu/refactoring/Rental.java
@@ -11,11 +11,32 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    public int getDaysRented() {
-        return daysRented;
+    public double getAmount() {
+        double thisAmount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 
-    public Movie getMovie() {
-        return movie;
+    public String getFigures() {
+        return "\t" + movie.getTitle() + "\t" + getAmount() + "\n";
+    }
+
+    public boolean isNewReleaseMovieAndRentedDaysMoreThan1() {
+        return (movie.getPriceCode() == Movie.NEW_RELEASE)
+                && daysRented > 1;
     }
 }
