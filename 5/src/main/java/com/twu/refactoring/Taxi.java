6b3481b178a071e9de305090a5b8358fdd54f51b
yan.zhao <catcatcarrot1207@163.com>
2021-08-15 10:22:43 +0800

    feature envy: move getTaxiCost to taxi

--- 5/src/main/java/com/twu/refactoring/Taxi.java
+++ 5/src/main/java/com/twu/refactoring/Taxi.java
@@ -1,9 +1,17 @@
 package com.twu.refactoring;
 
 public class Taxi {
-    private boolean airConditioned;
+    private final boolean airConditioned;
     private final int totalKms;
     private final boolean peakTime;
+
+    private static final double PEAK_TIME_MULTIPLIER = 1.2;
+    private static final double OFF_PEAK_MULTIPLIER = 1.0;
+    private static final int RATE_CHANGE_DISTANCE = 10;
+    private static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
+    private static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
+    private static final int PRE_RATE_CHANGE_AC_RATE = 20;
+    private static final int POST_RATE_CHANGE_AC_RATE = 17;
 
     public Taxi(boolean airConditioned, int totalKms, boolean peakTime) {
         this.airConditioned = airConditioned;
@@ -11,15 +19,17 @@ public class Taxi {
         this.peakTime = peakTime;
     }
 
-    public boolean isAirConditioned() {
-        return airConditioned;
-    }
-
-    public int getTotalKms() {
-        return totalKms;
-    }
-
-    public boolean isPeakTime() {
-        return peakTime;
+    public double getTaxiCharge() {
+        int totalKms = this.totalKms;
+        double peakTimeMultiple = peakTime ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+        double totalTaxiCharge = 0;
+        if (airConditioned) {
+            totalTaxiCharge += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalTaxiCharge += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+        } else {
+            totalTaxiCharge += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalTaxiCharge += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+        }
+        return totalTaxiCharge;
     }
 }
